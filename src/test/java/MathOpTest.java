5ee232e108707c1ff7b77f0d92186c376d7c00ab
OlgaChe <olgache73@gmail.com>
2014-06-22 22:15:25 +0300

    22June

--- src/test/java/MathOpTest.java
+++ src/test/java/MathOpTest.java
@@ -1,20 +1,68 @@
-import org.junit.*;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
 
 public class MathOpTest {
     private MathOp a;
 
+
     @Before
-    public void setUp(){
+    public void setUp() throws IOException {
+
         a = new MathOp();
+        }
+
+
+    @Test
+    public void testPos() {
+        String data = "3";
+        a.setScanner(new Scanner(data));
+        assertEquals(6, a.fac());
     }
 
     @Test
-    public void testFac() {
-        MathOp a = new MathOp();
-        assertEquals(6, a.fac(3));
+    public void testNeg() {
+        String data = "-3";
+        a.setScanner(new Scanner(data));
+        assertEquals(6, a.fac());
+    }
+
+    @Test
+    public void testOv() {
+        String data = "32";
+        a.setScanner(new Scanner(data));
+        assertEquals(6, a.fac());
+    }
+
+    @Test
+    public void testNull() {
+        String data = "0";
+        a.setScanner(new Scanner(data));
+        assertEquals(1, a.fac());
+    }
+
+    @Test
+    public void testDot() {
+        String data = "1,2";
+        a.setScanner(new Scanner(data));
+        assertEquals(6, a.fac());
+    }
+
+    @Test
+    public void testChar() {
+        String data = "e";
+        a.setScanner(new Scanner(data));
+        assertEquals(6, a.fac());
+    }
+
+    @Test
+    public void testIn() {
+        a.setScanner(new Scanner(System.in));
+        assertEquals(6, a.fac());
     }
 
 }
